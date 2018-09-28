a18847bce3b8fec700c42413d84f0ef166445ce5
Federico Orlandau <federico.orlandau@gmail.com>
2018-09-28 19:04:41 -0300

    Footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './style.css';
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <div className="footer">
+        Protojuego 2018
+      </div>
+    );
+  }
+}
--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { Container } from 'reactstrap';
+import Footer from 'components/Footer/Footer';
 import './style.css';
 
 export default class Layout extends Component {
   render() {
     return (
-      <Container className="container">
+      <Container fluid={true}>
         {this.props.children}
+        <Footer/>
       </Container>
     );
   }
