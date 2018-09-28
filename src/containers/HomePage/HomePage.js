58a86a9bca480fb0b865b1ac17896fbfdfa4d5e5
Federico Orlandau <federico.orlandau@gmail.com>
2018-09-28 18:29:48 -0300

    Login form

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { Random } from 'react-animated-text';
 import Layout from 'containers/Layout/Layout';
+import { Input, Button, Form, FormGroup } from 'reactstrap';
 import './style.css';
 
 export default class HomePageContainer extends Component {
@@ -8,7 +10,23 @@ export default class HomePageContainer extends Component {
     return (
       <Layout>
         <div className="logo-container">
-          <h1>ProtoJuego</h1>
+          <Random
+            text="Protojuego"
+            effect="jump"
+            effectChange={1.0}
+            effectDuration={0.5}
+          />
+        </div>
+        <div className="login-container">
+          <h3>Bienvenido!</h3>
+          <div className="form-container">
+          <Form>
+            <FormGroup>
+              <Input type="text" name="name" placeholder="Escribí tu nombre" />
+            </FormGroup>
+            <Button type="button" className="btn-protojuego">Entrar</Button>
+          </Form>
+          </div>
         </div>
       </Layout>
     );
