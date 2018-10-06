ed6655f0c10a762ce413be6cf17e0d33304d2912
Federico Orlandau <federico.orlandau@gmail.com>
2018-10-06 19:41:28 -0300

    Add container

--- src/components/Levels/Levels.js
+++ src/components/Levels/Levels.js
@@ -9,52 +9,51 @@ export default class LevelsComponenet extends Component {
     return (
       <div className="levels-container">
         <h3>Niveles</h3>
-        <Row>
-          <Col>
-            <Card>
-              <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
-              <CardBody>
-                <CardTitle>Card title</CardTitle>
-                <CardSubtitle>Card subtitle</CardSubtitle>
-                <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-                <Button>Button</Button>
-              </CardBody>
-            </Card>
-          </Col>
-          <Col>
-            <Card>
-              <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
-              <CardBody>
-                <CardTitle>Card title</CardTitle>
-                <CardSubtitle>Card subtitle</CardSubtitle>
-                <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-                <Button>Button</Button>
-              </CardBody>
-            </Card>
-          </Col>
-          <Col>
-            <Card>
-              <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
-              <CardBody>
-                <CardTitle>Card title</CardTitle>
-                <CardSubtitle>Card subtitle</CardSubtitle>
-                <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-                <Button>Button</Button>
-              </CardBody>
-            </Card>
-          </Col>
-          <Col>
-            <Card>
-              <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
-              <CardBody>
-                <CardTitle>Card title</CardTitle>
-                <CardSubtitle>Card subtitle</CardSubtitle>
-                <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-                <Button>Button</Button>
-              </CardBody>
-            </Card>
-          </Col>
-        </Row>
+        <hr/>
+        <div className="cards-container">
+          <Row>
+            <Col>
+              <Card>
+                <CardImg top width="100%" src="https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2018/07/Philips-AI-796x531.jpg" height="200" alt="Card image cap" />
+                <CardBody>
+                  <CardTitle>Simulacion</CardTitle>
+                  <CardText>Afronta los desafios </CardText>
+                  <Button>Ingresar</Button>
+                </CardBody>
+              </Card>
+            </Col>
+            <Col>
+              <Card>
+                <CardImg top width="100%" src="https://www.allnursingschools.com/wp-content/uploads/2016/11/article-nclex-exam.jpg" height="200" alt="Card image cap" />
+                <CardBody>
+                  <CardTitle>Examenes prácticos</CardTitle>
+                  <CardText>Afronta los desafios </CardText>
+                  <Button>Ingresar</Button>
+                </CardBody>
+              </Card>
+            </Col>
+            <Col>
+              <Card>
+                <CardImg top width="100%" src="https://lagunita.stanford.edu/c4x/StanfordOnline/O.P.E.N./asset/course_design_tile.jpg" height="200" alt="Card image cap" />
+                <CardBody>
+                  <CardTitle>Cursos</CardTitle>
+                  <CardText>Afronta los desafios </CardText>
+                  <Button>Ingresar</Button>
+                </CardBody>
+              </Card>
+            </Col>
+            <Col>
+              <Card>
+                <CardImg top width="100%" src="http://www.pewresearch.org/wp-content/uploads/2016/11/FT_16.11.23_Nonreaders-promo.jpg" height="200" alt="Card image cap" />
+                <CardBody>
+                  <CardTitle>Material de referencia</CardTitle>
+                  <CardText>Afronta los desafios </CardText>
+                  <Button>Ingresar</Button>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+        </div>
 
       </div>
     );
