5d4cdde4c5b05dbdbbb39213e692636208bfc53b
J. Nicolás Videla Rivero <nyko-94@hotmail.com>
2018-10-13 16:02:48 -0300

    remove duplicated questions for pm managment

--- src/constants/questions.js
+++ src/constants/questions.js
@@ -4,94 +4,18 @@ export const QUESTIONS = [
     options: [
       {
         answer: 'Documentar los requerimientos',
-        isRight: true
+        isRight: false
       },
       {
         answer: 'Estimar el tamaño del sistema',
-        isRight: true
+        isRight: false
       }, {
         answer: 'Todas son correctas',
         isRight: true
       }, {
         answer: 'Ninguna es correcta',
-        isRight: true
+        isRight: false
       }
     ],
-  },
-  {
-    question: 'Qué relacion de dependencia existe con los puntos de función?',
-    options: [
-      {
-        answer: 'Documentar los requerimientos',
-        isRight: true
-      },
-      {
-        answer: 'Estimar el tamaño del sistema',
-        isRight: true
-      }, {
-        answer: 'Todas son correctas',
-        isRight: true
-      }, {
-        answer: 'Ninguna es correcta',
-        isRight: true
-      }
-    ],
-  },
-  {
-    question: 'Qué son las transacciones de los casos de uso?',
-    options: [
-      {
-        answer: 'Documentar los requerimientos',
-        isRight: true
-      },
-      {
-        answer: 'Estimar el tamaño del sistema',
-        isRight: true
-      }, {
-        answer: 'Todas son correctas',
-        isRight: true
-      }, {
-        answer: 'Ninguna es correcta',
-        isRight: true
-      }
-    ],
-  },
-  {
-    question: 'Qué tipo de estimacion permite obtener los puntos de función?',
-    options: [
-      {
-        answer: 'Documentar los requerimientos',
-        isRight: true
-      },
-      {
-        answer: 'Estimar el tamaño del sistema',
-        isRight: true
-      }, {
-        answer: 'Todas son correctas',
-        isRight: true
-      }, {
-        answer: 'Ninguna es correcta',
-        isRight: true
-      }
-    ],
-  },
-  {
-    question: 'Qué presenta un caso de uso?',
-    options: [
-      {
-        answer: 'Documentar los requerimientos',
-        isRight: true
-      },
-      {
-        answer: 'Estimar el tamaño del sistema',
-        isRight: true
-      }, {
-        answer: 'Todas son correctas',
-        isRight: true
-      }, {
-        answer: 'Ninguna es correcta',
-        isRight: true
-      }
-    ],
-  },
+  }
 ];
