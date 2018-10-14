94e41bf9af1ca4cbd1915895147ba6950f1bebb6
Nicolás <videlariveronicolas@gmail.com>
2018-10-14 17:19:16 -0300

    Update the routes to add the missing one

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -6,5 +6,10 @@ export const ROUTES = {
     TED_TALKS: '/ted_talks',    //Punto 4 TP
     GESTION_CAMBIO: '/gestion_cambio',  // Punto 6 TP
     GESTION_CAMBIO_QUIZ: '/gestion_cambio_quiz', // Punto 6 TP
-    QUESTIONS: '/examen'    //Punto 7 TP 
+    QUESTIONS: '/examen',    //Punto 7 TP 
+    METRICAS_SOFTWARE: '/metricas_software',
+    METRICAS_SOFTWARE_QUIZ: '/metricas_software_quiz',
+    ESTIMACIONES: '/estimaciones',
+    ESTIMACIONES_QUIZ: '/estimaciones_quiz',
+    DIPLOMA: '/diploma'
 };
