0f4b0b9f5566d581c7a626cee2f4fb03923c632b
J. Nicolás Videla Rivero <nyko-94@hotmail.com>
2018-10-13 22:16:57 -0300

    Add a few real questions to change managment

--- src/constants/questionsGestionCambio.js
+++ src/constants/questionsGestionCambio.js
@@ -1,21 +1,103 @@
 export const QUESTIONS = [
   {
-    question: 'Qué es un cambio?',
+    question: '¿Quienes son los desviadores positivos?',
     options: [
       {
-        answer: 'viva la pepa?',
+        answer: 'Personas o grupos que no son afectados por las problemáticas',
         isRight: false
       },
       {
-        answer: 'Estar atr?',
+        answer: 'Personas afectadas por las problemáticas',
+        isRight: false
+      }, {
+        answer: 'Personas o grupos afectados por las mismas restricciones, pero que producen buenos resultados',
+        isRight: true
+      }, {
+        answer: 'Personas o grupos más afectados por las restricciones',
+        isRight: false
+      }
+    ]
+  },
+  {
+    question: 'Según el primer pasos que propone el enfoque de desviación positiva. ¿Porque se debe convertir al grupo en Gurú?',
+    options: [
+      {
+        answer: 'Para generar dependencias sobre los líderes',
+        isRight: false
+      },
+      {
+        answer: 'Para que la desconfianza y la resistencia sean más fáciles de superar',
+        isRight: true
+      }, {
+        answer: 'Para que ningún integrante dependa de nadie',
+        isRight: false
+      }, {
+        answer: 'Ninguna es correcta',
+        isRight: false
+      }
+    ]
+  },
+  {
+  q3: '',
+  q3: ['', '', '', 'Todas las anteriores son correctas'],
+  q3: '',
+    question: 'Según el enfoque de desviación positiva. ¿Cuál es la causa de que algunos cambios no puedan realizarse?',
+    options: [
+      {
+        answer: 'La gente se aferra al statu quo',
+        isRight: false
+      },
+      {
+        answer: 'Los desviadores positivos podrían tener miedo a quedar expuestos',
+        isRight: false
+      }, {
+        answer: 'Las soluciones desarrolladas externamente poseen mayor rechazo',
+        isRight: false
+      }, {
+        answer: 'Todas las anteriores son correctas',
+        isRight: true
+      }
+    ]
+  },
+  {
+    question: 'Según el enfoque de desviación positiva ¿Cuáles son algunas de las funciones del líder?',
+    options: [
+      {
+        answer: 'Gestionar la atención y asignar los recursos escasos',
+        isRight: true
+      },
+      {
+        answer: 'Imponer sus ideas sobre los demás',
+        isRight: false
+      }, {
+        answer: 'Gestionar la atención e imponer la participación de los desviadores positivos',
+        isRight: false
+      }, {
+        answer: 'Todas son correctas',
+        isRight: false
+      }
+    ]
+  },
+  { 
+    question: '¿Cuando es conveniente utilizar el enfoque de desviación positiva?',
+    options: [
+      {
+        answer: 'Cuando se requieren cambios de comportamiento y actitud',
+        isRight: false
+      },
+      {
+        answer: 'Cuando no existen soluciones genéricas evidentes',
+        isRight: false
+      }, {
+        answer: 'Cuando las estrategias exitosas permanecen aisladas y ocultas',
         isRight: false
       }, {
         answer: 'Todas son correctas',
         isRight: true
-      }, {
-        answer: 'La respuesta está en tu corazón',
-        isRight: false
       }
     ]
-  }
+  },
+
+
 ];
+
