acb7ce6fe781d5d4a30d7e1a541a7f4122abfbcc
Federico Orlandau <federico.orlandau@gmail.com>
2018-10-20 16:27:21 -0400

    Fix GQM

--- src/components/AplicacionGQM/AplicacionGQM.js
+++ src/components/AplicacionGQM/AplicacionGQM.js
@@ -1,13 +1,62 @@
 import React, { Component } from 'react';
 import 'react-toastify/dist/ReactToastify.css';
-import './logic.js'
 import 'rc-slider/assets/index.css';
 import './style.css'
+import Tree from 'react-animated-tree'
+
+const treeStyles = {
+  position: 'absolute',
+  top: 40,
+  left: 40,
+  color: 'white',
+  fill: 'white',
+  width: '100%'
+}
+
+const typeStyles = {
+  fontSize: '2em',
+  verticalAlign: 'middle'
+}
+
 
 export default class AplicacionGQMComponent extends Component {
   render() {
     return (
-    <div id= "aplicacionGQM-container"></div>
+    <div>
+      <Tree content="CASOS DE ESTUDIO" canHide open style={treeStyles}>
+        <Tree content="TERADYNE" type={<span style={typeStyles}>🙀</span>} canHide />
+        <Tree content="FVL" canHide>
+          <Tree content="GOAL">
+            <Tree content="PROBLEMA 1" style={{ color: '#63b1de' }}>
+              <Tree content="METRICA 1" canHide />
+              <Tree content="METRICA 2" canHide />
+              <Tree content="METRICA 3" canHide />
+            </Tree>
+            <Tree content="PROBLEMA 2" style={{ color: '#63b1de' }}>
+              <Tree content="METRICA 1" canHide />
+              <Tree content="METRICA 2" canHide />
+              <Tree content="METRICA 3" canHide />
+            </Tree>
+            <Tree content="PROBLEMA 3" style={{ color: '#63b1de' }}>
+              <Tree content="METRICA 1" canHide />
+              <Tree content="METRICA 2" canHide />
+              <Tree content="METRICA 3" canHide />
+            </Tree>
+            <Tree content="PROBLEMA 4" style={{ color: '#63b1de' }}>
+              <Tree content="METRICA 1" canHide />
+              <Tree content="METRICA 2" canHide />
+              <Tree content="METRICA 3" canHide />
+            </Tree>
+            <Tree content="PROBLEMA 5" style={{ color: '#63b1de' }}>
+              <Tree content="METRICA 1" canHide />
+              <Tree content="METRICA 2" canHide />
+              <Tree content="METRICA 3" canHide />
+            </Tree>
+          </Tree>
+        </Tree>
+        <Tree content="CISCO" canHide />
+      </Tree>
+    </div>
     );
   }
 }
