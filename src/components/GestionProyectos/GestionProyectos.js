a0c269b8bc045cc9bc4f62b5e67a7606ae37847a
luke92 <lucasjv92@gmail.com>
2018-10-22 00:39:18 -0300

    Se agregaron papers

--- src/components/GestionProyectos/GestionProyectos.js
+++ src/components/GestionProyectos/GestionProyectos.js
@@ -53,8 +53,53 @@ export default class GestionProyectosComponent extends Component {
                     </a>
                 </div>
                 <div className="linksGestionProyectos" id="paper2">
-                    <a id="gestionProyectosLink2" onClick={() => this.cargarPaper(document.getElementById("gestionProyectosLink2").text, "https://www.youtube.com/embed/rrkrvAUbU9Y", "https://docs.google.com/document/d/e/2PACX-1vTiFjj-kXPgF-hR3ll-kdev4icEIIXLODp33x_zmgQur8rs7UkInzilVmaKsWcoY6B6i-dpnp6bifu-/pub?embedded=true", "https://docs.google.com/document/d/e/2PACX-1vRsxQ0IfHTfiIFYMBFFywoKbbxJbiAd0rEixnbbY5OtKOHTFxuoxrXAPZvcc1LbMZqtiPyBARj80zSt/pub?embedded=true" )}>
-                    paper 2
+                <a id="gestionProyectosLink2" onClick={() => 
+                      this.cargarPaper(//titulo, urlPaper, UrlResumen, urlRelaciones
+                        document.getElementById("gestionProyectosLink2").text, 
+                        "https://docs.google.com/document/d/e/2PACX-1vT2k91xlMSOcQ7tLF0tEK0XKWcfkLqhoCZCnoIfZRGvFN7zRUUN_qwQIzbk7d6PZyrc5eWmKGol14se/pub?embedded=true", 
+                        "https://docs.google.com/document/d/e/2PACX-1vR_-EHkveg4ZdGKVnXdrMSO2-X5e12ka8Vgs61AdxuLZCalfaRfgucuw04AG43oRo5Nn3vMAG7fJa2q/pub?embedded=true",
+                        "https://docs.google.com/document/d/e/2PACX-1vQC_g3YhEgG4ZcS7PSBjXeD5HaTlCek0G7EETI2Cdx5__HqxXdZiTk8bF6Qp9jt2hXhYfBeS6Y0nKT_/pub?embedded=true" )}>
+                        Leading multigenerational projects and surviving
+                    </a>
+                </div>
+                <div className="linksGestionProyectos" id="paper3">
+                <a id="gestionProyectosLink3" onClick={() => 
+                      this.cargarPaper(//titulo, urlPaper, UrlResumen, urlRelaciones
+                        document.getElementById("gestionProyectosLink3").text, 
+                        "https://docs.google.com/document/d/e/2PACX-1vS8zcYplM8jdYMBVEDEjyCicQp6Xh-XBOmwAnTxJpG-B0Eq83IwmtdkJ99TjkT6RkKnApzAcJB5RHFH/pub?embedded=true", 
+                        "https://docs.google.com/document/d/e/2PACX-1vTJvVuDDq-lmGF4wZENB3hQKer86-RxEoSzB16XkcOX13yc6su_NCfh3yDKgLxzhLFteB7jemS5stT7/pub?embedded=true",
+                        "https://docs.google.com/document/d/e/2PACX-1vRsxQ0IfHTfiIFYMBFFywoKbbxJbiAd0rEixnbbY5OtKOHTFxuoxrXAPZvcc1LbMZqtiPyBARj80zSt/pub?embedded=true" )}>
+                        Agile Software Development: The Business of Innovation
+                    </a>
+                </div>
+                <div className="linksGestionProyectos" id="paper4">
+                <a id="gestionProyectosLink4" onClick={() => 
+                      this.cargarPaper(//titulo, urlPaper, UrlResumen, urlRelaciones
+                        document.getElementById("gestionProyectosLink4").text, 
+                        "https://docs.google.com/document/d/e/2PACX-1vSJUUPnDX5EgupZaL5KsYt8Ybqsts5sgx6ZF5gJIMpnYkurlBpQxaC53Da2OvYS_aIZEGcc1SqowDOQ/pub?embedded=true", 
+                        "https://docs.google.com/document/d/e/2PACX-1vRjGSRrj04tD4K5hyow05e-TGLyf3jroHGpu6Tfv_F9B7gsFoXwzor6zPEzZVAngEt_42ZE7oxlj2_o/pub?embedded=true",
+                        "https://docs.google.com/document/d/e/2PACX-1vQEMBHUk5N2kG87kmJL3GNHxUgKIKmHDO7UcTSrLrvn9mD8ONAz4LzwuZ4OtPSwOtZ3tvjrjg83FHcP/pub?embedded=true" )}>
+                        Using emotional intelligence to improve project performance
+                    </a>
+                </div>
+                <div className="linksGestionProyectos" id="paper5">
+                <a id="gestionProyectosLink5" onClick={() => 
+                      this.cargarPaper(//titulo, urlPaper, UrlResumen, urlRelaciones
+                        document.getElementById("gestionProyectosLink5").text, 
+                        "https://docs.google.com/document/d/e/2PACX-1vT9y67HyPudMTA2dsxW5FgtQHTWnGAQLm04Y67ha-2AUKk6VsTb7xHzjQKaIUvm8vX9Kotk0EPS0q_O/pub?embedded=true", 
+                        "https://docs.google.com/document/d/e/2PACX-1vS4zUym_zI-4T6cyERkF_qq2VT7qqjhDETD4DR1DSzjbBxIx4sM_Ts3isTQGGxtLXJbrgYL_yWgtaM-/pub?embedded=true",
+                        "https://docs.google.com/document/d/e/2PACX-1vTrhv03_SAaWtGyB6l8t-qzf5eLJRCRiWhvlRZerl_7gWZ9HZD90-sHnUI_EgxM8Dd2K0UITsDBYKUN/pub?embedded=true" )}>
+                        Managing multicultural teams (CÓMO GERENCIAR EQUIPOS MULTICULTURALES)
+                    </a>
+                </div>
+                <div className="linksGestionProyectos" id="paper6">
+                <a id="gestionProyectosLink6" onClick={() => 
+                      this.cargarPaper(//titulo, urlPaper, UrlResumen, urlRelaciones
+                        document.getElementById("gestionProyectosLink6").text, 
+                        "https://docs.google.com/document/d/e/2PACX-1vQyOSMQBobPHan478uDrJUZAH9IrGgzIswxMs5OIC-vF61Ggc7LgOVvekefYTP4V9DWnhzRGl-w8FKd/pub?embedded=true", 
+                        "https://docs.google.com/document/d/e/2PACX-1vQwaiNsxwaUkViGQzn0Ahx89DfcroBYirxQJaKGs1O9d-yasPONI3JCdw7mwd5-6y1prQJ_UoN-AOBE/pub?embedded=true",
+                        "https://docs.google.com/document/d/e/2PACX-1vQBPJsjd0dxe9onPqNSjwvuxX2F-nhc9EybQ42t9nDro80T1oAdT7MUK_bI_4eEYemK9GIxg2zItLbE/pub?embedded=true" )}>
+                        Wearable technology to understand humans (Neurotecnologías, la necesidad de un compromiso ético en su implementación)
                     </a>
                 </div>
             </div>
