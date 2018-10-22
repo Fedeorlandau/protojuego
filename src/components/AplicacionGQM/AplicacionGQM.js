1891ca45627ed4ef4b6cec8e1ebd952b8331813e
Gpesado <gonzapesa@gmail.com>
2018-10-22 15:07:43 +0100

    FIX TITULOS GQM, AGREGADOS COLORES PARA DIFERENCIAR GQM

--- src/components/AplicacionGQM/AplicacionGQM.js
+++ src/components/AplicacionGQM/AplicacionGQM.js
@@ -6,8 +6,8 @@ import Tree from 'react-animated-tree'
 
 const treeStyles = {
   position: 'absolute',
-  top: 80,
-  left: 80,
+  //top: 80,
+  //left: 80,
   color: 'white',
   fill: 'white',
   width: '100%'
@@ -22,92 +22,92 @@ export default class AplicacionGQMComponent extends Component {
     <br></br>
     </div>
       <Tree content="CASOS DE ESTUDIO" open style={treeStyles}>
-      <Tree content="QUATEAMS" >
-          <Tree content="G: Poder gestionar de forma dinámica los miembros del IIF desde la aplicación.">
-            <Tree content="Q: ¿El sistema contempla el crecimiento abrupto de usuarios?" style={{ color: '#63b1de' }}>
-              <Tree content="M: Total de miembros activos en la aplicación."  />
-              <Tree content="M: Porcentaje de aumento de miembros por mes."  />
-              <Tree content="M: Estimaciones sobre el crecimiento paulatino de miembros."  />
+      <Tree content="Quateams CRM: procesos" type="CASO">
+          <Tree content="Poder gestionar de forma dinámica los miembros del IIF desde la aplicación." type="GOAL" style={{ color: 'GREEN' }}>
+            <Tree content="¿El sistema contempla el crecimiento abrupto de usuarios?" type="QUESTION" style={{ color: '#63b1de' }}>
+              <Tree content="Total de miembros activos en la aplicación."  type="METRICS" style={{ color: 'GREY' }} />
+              <Tree content="Porcentaje de aumento de miembros por mes."   type="METRICS" style={{ color: 'GREY' }} />
+              <Tree content="Estimaciones sobre el crecimiento paulatino de miembros."  type="METRICS" style={{ color: 'GREY' }} />
             </Tree>
-            <Tree content="Q: ¿El acceso a la información es relativamente rápido por parte de los miembros?" style={{ color: '#63b1de' }}>
-              <Tree content="M: Tiempo de respuesta de cada consulta a la base de datos."  />
-              <Tree content="M: Tablas a las que se accede con mayor frecuencia en la Base de Datos. "  />
-              <Tree content="M: Cantidad de consultas simultáneas soportadas por el servidor."  />
-              <Tree content="M: Costos mensuales/anuales de los servidores."  />
+            <Tree content="¿El acceso a la información es relativamente rápido por parte de los miembros?" type="QUESTION" style={{ color: '#63b1de' }}>
+              <Tree content="Tiempo de respuesta de cada consulta a la base de datos." type="METRICS" style={{ color: 'GREY' }} />
+              <Tree content="Tablas a las que se accede con mayor frecuencia en la Base de Datos. " type="METRICS" style={{ color: 'GREY' }} />
+              <Tree content="Cantidad de consultas simultáneas soportadas por el servidor." type="METRICS" style={{ color: 'GREY' }} />
+              <Tree content="Costos mensuales/anuales de los servidores." type="METRICS" style={{ color: 'GREY' }} />
             </Tree>
-            <Tree content="Q: ¿Se contempla dentro del sistema el acceso de usuarios provenientes del exterior?" style={{ color: '#63b1de' }}>
-              <Tree content="M: Cantidad de miembros activos en la aplicación."  />
-              <Tree content="M: Cantidad de usuarios provenientes del exterior."  />
-              <Tree content="M: Tiempos de respuesta del servidor."  />
-              <Tree content="M: Cantidad de consultas simultáneas soportadas por el servidor."  />
+            <Tree content="¿Se contempla dentro del sistema el acceso de usuarios provenientes del exterior?" type="QUESTION" style={{ color: '#63b1de' }}>
+              <Tree content="Cantidad de miembros activos en la aplicación." type="METRICS" style={{ color: 'GREY' }} />
+              <Tree content="Cantidad de usuarios provenientes del exterior." type="METRICS" style={{ color: 'GREY' }} />
+              <Tree content="Tiempos de respuesta del servidor." type="METRICS" style={{ color: 'GREY' }} />
+              <Tree content="Cantidad de consultas simultáneas soportadas por el servidor." type="METRICS" style={{ color: 'GREY' }} />
             </Tree>
-            <Tree content="Q: ¿A qué información acceden los miembros?" style={{ color: '#63b1de' }}>
-              <Tree content="M: Número de visitas de cada documento publicado."  />
-              <Tree content="M: Cantidad de audiencias por mes realizadas por la IIF."  />
+            <Tree content="¿A qué información acceden los miembros?" type="QUESTION" style={{ color: '#63b1de' }}>
+              <Tree content="Número de visitas de cada documento publicado." type="METRICS" style={{ color: 'GREY' }} />
+              <Tree content="Cantidad de audiencias por mes realizadas por la IIF." type="METRICS" style={{ color: 'GREY' }} />
             </Tree>
-            <Tree content="Q: ¿Saben los miembros cuál es su estado de cuenta?" style={{ color: '#63b1de' }}>
-              <Tree content="M: Tablas a las que se accede con mayor frecuencia en la Base de Datos. "  />
-              <Tree content="M: Tiempo que se tarda en consultar los pagos."  />
-              <Tree content="M: Número de pagos registrados."  />
+            <Tree content="¿Saben los miembros cuál es su estado de cuenta?" type="QUESTION" style={{ color: '#63b1de' }}>
+              <Tree content="Tablas a las que se accede con mayor frecuencia en la Base de Datos. " type="METRICS" style={{ color: 'GREY' }} />
+              <Tree content="Tiempo que se tarda en consultar los pagos." type="METRICS" style={{ color: 'GREY' }} />
+              <Tree content="Número de pagos registrados." type="METRICS" style={{ color: 'GREY' }} />
             </Tree>
           </Tree>
         </Tree>
-        <Tree content="FVL">
-          <Tree content= "G: Mejorar el tiempo de acceso a las historias clínicas de los pacientes del hospital desde el punto de vista de los médicos y personal administrativo">
-            <Tree content="Q: ¿Cómo se realiza el trabajo administrativo actualmente?" style={{ color: '#63b1de' }}>
-              <Tree content="M: Cantidad de personal dedicado a tareas administrativas." />
-              <Tree content="M: Cantidad de formularios a llenar por cada tipo de personal." />
-              <Tree content="M: Tiempo que le demanda a cada usuario realizar una tarea administrativa." />
+        <Tree content="FVL: recursos" type="CASO">
+          <Tree content="Mejorar el tiempo de acceso a las historias clínicas de los pacientes del hospital desde el punto de vista de los médicos y personal administrativo" type="GOAL" style={{ color: 'GREEN' }} >
+            <Tree content="¿Cómo se realiza el trabajo administrativo actualmente?" type="QUESTION" style={{ color: '#63b1de' }}>
+              <Tree content="Cantidad de personal dedicado a tareas administrativas."  type="METRICS" style={{ color: 'GREY' }} />
+              <Tree content="Cantidad de formularios a llenar por cada tipo de personal."  type="METRICS" style={{ color: 'GREY' }} />
+              <Tree content="Tiempo que le demanda a cada usuario realizar una tarea administrativa."  type="METRICS" style={{ color: 'GREY' }} />
             </Tree>
-            <Tree content="Q: ¿Cómo son las interacciones entre el personal del hospital?" style={{ color: '#63b1de' }}>
-              <Tree content="M: Tiempo empleado en la comunicación." />
-              <Tree content="M: Cantidad de empleados involucrados en cada tarea." />
-              <Tree content="M: Cantidad de formularios a llenar por cada tipo de personal." />
+            <Tree content="¿Cómo son las interacciones entre el personal del hospital?" type="QUESTION" style={{ color: '#63b1de' }}>
+              <Tree content="Tiempo empleado en la comunicación."  type="METRICS" style={{ color: 'GREY' }} />
+              <Tree content="Cantidad de empleados involucrados en cada tarea."  type="METRICS" style={{ color: 'GREY' }} />
+              <Tree content="Cantidad de formularios a llenar por cada tipo de personal."  type="METRICS" style={{ color: 'GREY' }} />
             </Tree>
-            <Tree content="Q: ¿Qué mejoras puede implicar la sistematización de las HC?" style={{ color: '#63b1de' }}>
-              <Tree content="M: Estimación de documentos extraviados."  />
-              <Tree content="M: Frecuencia con las que se utiliza cada historia clínica."  />
-              <Tree content="M: Promedio de errores de interpretación de documentos."  />
+            <Tree content="¿Qué mejoras puede implicar la sistematización de las HC?" type="QUESTION" style={{ color: '#63b1de' }}>
+              <Tree content="Estimación de documentos extraviados." type="METRICS" style={{ color: 'GREY' }} />
+              <Tree content="Frecuencia con las que se utiliza cada historia clínica." type="METRICS" style={{ color: 'GREY' }} />
+              <Tree content="Promedio de errores de interpretación de documentos." type="METRICS" style={{ color: 'GREY' }} />
             </Tree>
-            <Tree content="Q: ¿Cómo pueden llegar a reaccionar los usuarios ante el nuevo sistema?" style={{ color: '#63b1de' }}>
-              <Tree content="M: Edad promedio de los médicos del hospital." />
-              <Tree content="M: Tiempo de antigüedad promedio del personal del hospital." />
+            <Tree content="¿Cómo pueden llegar a reaccionar los usuarios ante el nuevo sistema?" type="QUESTION" style={{ color: '#63b1de' }}>
+              <Tree content="Edad promedio de los médicos del hospital."  type="METRICS" style={{ color: 'GREY' }} />
+              <Tree content="Tiempo de antigüedad promedio del personal del hospital."  type="METRICS" style={{ color: 'GREY' }} />
             </Tree>
-            <Tree content="Q: ¿Cuánto dinero se puede ahorrar con la sistematización de la facturación?" style={{ color: '#63b1de' }}>
-              <Tree content="M: Cantidad de insumos utilizados en las cirugías." />
-              <Tree content="M: Cantidad de facturas rechazadas por aseguradoras." />
+            <Tree content="¿Cuánto dinero se puede ahorrar con la sistematización de la facturación?" type="QUESTION" style={{ color: '#63b1de' }}>
+              <Tree content="Cantidad de insumos utilizados en las cirugías."  type="METRICS" style={{ color: 'GREY' }} />
+              <Tree content="Cantidad de facturas rechazadas por aseguradoras."  type="METRICS" style={{ color: 'GREY' }} />
             </Tree>
           </Tree>
-        </Tree><Tree content="TERADYNE">
-          <Tree content= "G: Mejorar la precisión de las estimaciones de desarrollo mediante el uso de una herramienta de gestión de proyectos desde el punto de vista del Project manager.">
-            <Tree content="Q: ¿Cuánto desvío de tiempo tienen los proyectos desarrollados actualmente?" style={{ color: '#63b1de' }}>
-              <Tree content="M: Porcentaje de proyectos con desvío utilizando herramientas de gestión de proyectos."  />
-              <Tree content="M: Porcentaje de proyectos con desvío sin utilizar herramientas de gestión de proyectos."  />
+        </Tree><Tree content="Teradyne: producto" type="CASO">
+          <Tree content="Mejorar la precisión de las estimaciones de desarrollo mediante el uso de una herramienta de gestión de proyectos desde el punto de vista del Project manager." type="GOAL" style={{ color: 'GREEN' }}>
+            <Tree content="¿Cuánto desvío de tiempo tienen los proyectos desarrollados actualmente?" type="QUESTION" style={{ color: '#63b1de' }}>
+              <Tree content="Porcentaje de proyectos con desvío utilizando herramientas de gestión de proyectos." type="METRICS" style={{ color: 'GREY' }} />
+              <Tree content="Porcentaje de proyectos con desvío sin utilizar herramientas de gestión de proyectos." type="METRICS" style={{ color: 'GREY' }} />
             </Tree>
-            <Tree content="Q: ¿El uso de una herramienta de gestión de proyectos mejora la estimación de los proyectos?" style={{ color: '#63b1de' }}>
-              <Tree content="M: Cantidad de proyectos con desvío (umbral de desvío mayor al 5%) utilizando la herramienta." />
-              <Tree content="M: Promedio de horas de trabajo dedicadas a la utilización de la herramienta en cada proyecto. (Overhead)" />
-              <Tree content="M: Porcentaje de tareas estimadas como cuello de botella vs. tareas reales." />
-              <Tree content="M: Porcentaje de desvío en estimación de tres puntos" />
+            <Tree content="¿El uso de una herramienta de gestión de proyectos mejora la estimación de los proyectos?" type="QUESTION" style={{ color: '#63b1de' }}>
+              <Tree content="Cantidad de proyectos con desvío (umbral de desvío mayor al 5%) utilizando la herramienta."  type="METRICS" style={{ color: 'GREY' }} />
+              <Tree content="Promedio de horas de trabajo dedicadas a la utilización de la herramienta en cada proyecto. (Overhead)"  type="METRICS" style={{ color: 'GREY' }} />
+              <Tree content="Porcentaje de tareas estimadas como cuello de botella vs. tareas reales."  type="METRICS" style={{ color: 'GREY' }} />
+              <Tree content="Porcentaje de desvío en estimación de tres puntos"  type="METRICS" style={{ color: 'GREY' }} />
             </Tree>
-            <Tree content="Q: ¿Cuánto aumento de costos tienen los proyectos que utilizan la herramienta de gestión de proyectos?" style={{ color: '#63b1de' }}>
-              <Tree content="M: Porcentaje de proyectos con desvío utilizando herramientas de gestión de proyectos."  />
-              <Tree content="M: Promedio de reasignación de personal a proyectos en curso."  />
-              <Tree content="M: Cantidad de horas extras pagadas."  />
-              <Tree content="M: Cantidad de horas de capacitación en la herramienta de gestión de proyectos." />
+            <Tree content="¿Cuánto aumento de costos tienen los proyectos que utilizan la herramienta de gestión de proyectos?" type="QUESTION" style={{ color: '#63b1de' }}>
+              <Tree content="Porcentaje de proyectos con desvío utilizando herramientas de gestión de proyectos." type="METRICS" style={{ color: 'GREY' }} />
+              <Tree content="Promedio de reasignación de personal a proyectos en curso." type="METRICS" style={{ color: 'GREY' }} />
+              <Tree content="Cantidad de horas extras pagadas." type="METRICS" style={{ color: 'GREY' }} />
+              <Tree content="Cantidad de horas de capacitación en la herramienta de gestión de proyectos."  type="METRICS" style={{ color: 'GREY' }} />
             </Tree>
-            <Tree content="Q: ¿Qué problemas puede generar la utilización de una herramienta de gestión de proyectos?" style={{ color: '#63b1de' }}>
-              <Tree content="M: Cantidad de proyectos con desvío (umbral de desvío mayor al 5%) utilizando la herramienta."  />
-              <Tree content="M: Porcentaje de tareas estimadas como cuello de botella vs. tareas reales."  />
-              <Tree content="M: Porcentaje de desvío en estimación de tres puntos."  />
-              <Tree content="M: Porcentaje de horas insumidas en la utilización de la herramienta de gestión de proyectos por proyecto."  />
-              <Tree content="M: Cantidad de interpretaciones erróneas subjetivas por parte del project manager de la herramienta por proyecto."  />
+            <Tree content="¿Qué problemas puede generar la utilización de una herramienta de gestión de proyectos?" type="QUESTION" style={{ color: '#63b1de' }}>
+              <Tree content="Cantidad de proyectos con desvío (umbral de desvío mayor al 5%) utilizando la herramienta." type="METRICS" style={{ color: 'GREY' }} />
+              <Tree content="Porcentaje de tareas estimadas como cuello de botella vs. tareas reales." type="METRICS" style={{ color: 'GREY' }} />
+              <Tree content="Porcentaje de desvío en estimación de tres puntos." type="METRICS" style={{ color: 'GREY' }} />
+              <Tree content="Porcentaje de horas insumidas en la utilización de la herramienta de gestión de proyectos por proyecto." type="METRICS" style={{ color: 'GREY' }} />
+              <Tree content="Cantidad de interpretaciones erróneas subjetivas por parte del project manager de la herramienta por proyecto." type="METRICS" style={{ color: 'GREY' }} />
             </Tree>
-            <Tree content="Q: ¿La herramienta de gestión de proyectos permite identificar desvíos a tiempo?" style={{ color: '#63b1de' }}>
-              <Tree content="M: Cantidad de proyectos con desvío (umbral de desvío mayor al 5%) utilizando la herramienta."  />
-              <Tree content="M: Porcentaje de tareas estimadas como cuello de botella vs. tareas reales."  />
-              <Tree content="M: Cantidad de interpretaciones erróneas subjetivas por parte del project manager de la herramienta por proyecto."  />
-              <Tree content="M: Cantidad de re-estimaciones realizadas en proyectos que utilizan herramienta de gestión de proyectos."  />
+            <Tree content="¿La herramienta de gestión de proyectos permite identificar desvíos a tiempo?" type="QUESTION" style={{ color: '#63b1de' }}>
+              <Tree content="Cantidad de proyectos con desvío (umbral de desvío mayor al 5%) utilizando la herramienta." type="METRICS" style={{ color: 'GREY' }} />
+              <Tree content="Porcentaje de tareas estimadas como cuello de botella vs. tareas reales." type="METRICS" style={{ color: 'GREY' }} />
+              <Tree content="Cantidad de interpretaciones erróneas subjetivas por parte del project manager de la herramienta por proyecto." type="METRICS" style={{ color: 'GREY' }} />
+              <Tree content="Cantidad de re-estimaciones realizadas en proyectos que utilizan herramienta de gestión de proyectos." type="METRICS" style={{ color: 'GREY' }} />
             </Tree>
           </Tree>
         </Tree>
