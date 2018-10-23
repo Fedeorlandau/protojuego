1e16371a1910b35b04569ada05efb13959c04a3f
luke92 <lucasjv92@gmail.com>
2018-10-23 01:21:34 -0300

    Cambios en cards, gestion de proyectos, volver atras

--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
 import { Container } from 'reactstrap';
 import Footer from 'components/Footer/Footer';
+import history from 'history/history'
 import './style.css';
 
 export default class Layout extends Component {
+
+
   render() {
     return (
+      
       <Container fluid={true} className="h-100">
+        <span
+          className="arrow-left" 
+          onClick={() => history.push('/dashboard')}>
+          <img alt="volver" className="arrow-left" src={require('./chevron-sign-left.png')} />
+        Volver al Dashboard
+        </span>
         {this.props.children}
         {!this.props.showFooter && <Footer/> }
       </Container>
+      
     );
   }
 }
